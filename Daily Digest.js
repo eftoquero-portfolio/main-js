7f813d2929327cda6f06b6649505a316565b0d33
fraxxm <49774697+fraxxm@users.noreply.github.com>
2019-09-20 01:14:13 +0800

    Update Daily Digest.js

--- Daily Digest.js
+++ Daily Digest.js
@@ -31,3 +31,16 @@ $('a.right-b').click(function() {
     scrollLeft: "-=500px"
   }, "normal");
 });
+
+// Password
+console.clear();
+
+const elInput = document.querySelector('#inputpassword');
+const elDisplay = document.querySelector('#displaypassword');
+
+Splitting({ whitespace: true });
+elInput.addEventListener('inputpassword',()=>{
+  elDisplay.innerHTML = Splitting.html({ content: elInput.value, whitespace: true });
+});
+
+window.setTimeout(()=>{ document.querySelector('#revealpassword').click() }, 99999);
